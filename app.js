1d238c8ab1356e341db66afabf7ffc124023fbb5
PTCL1 <erp@ptcl.co.in>
2025-12-17 16:43:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ====== CONFIG ======
-const API_URL = 'https://script.google.com/a/macros/ptcl.co.in/s/AKfycbxVM_euuBkWOhjzRWfushPeNoDgnkmj5LnrwVV-0ZJC7ZfVBnuRsoKZ0gRTIwGMFl65OA/exec'; // e.g. https://script.google.com/macros/s/AKfycbx.../exec
+const API_URL = 'https://script.google.com/a/macros/AKfycbxVM_euuBkWOhjzRWfushPeNoDgnkmj5LnrwVV-0ZJC7ZfVBnuRsoKZ0gRTIwGMFl65OA/exec'; // e.g. https://script.google.com/macros/s/AKfycbx.../exec
 
 async function api(action, data = {}) {
   const body = new URLSearchParams({ action, ...data });
